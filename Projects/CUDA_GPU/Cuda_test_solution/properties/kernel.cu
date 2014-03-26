04a7f7105dc12180d0073bad47f55e648f2ff092
Tianyi <wty1990000@yeah.net>
2014-03-26 18:38:35 +0800

    CUDA stream test

---
+++ Projects/CUDA_GPU/Cuda_test_solution/properties/kernel.cu
@@ -0,0 +1,21 @@
+
+#include <cassert>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+int main()
+{
+	int dev_count;
+	cudaDeviceProp prop;
+	cudaGetDeviceCount( &dev_count);
+	for (int i = 0; i < dev_count; i++) {
+		cudaGetDeviceProperties(&prop, i);
+	}
+	if (prop.deviceOverlap){
+		printf("Device support CUDA streams");
+	}
+	
+	return 0;
+}
