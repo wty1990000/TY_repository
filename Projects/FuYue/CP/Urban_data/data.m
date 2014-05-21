3cebc3b553f012027ba17e80f6a832260b6f2b22
Tianyi <wty1990000@yeah.net>
2014-05-21 11:38:43 +0800

    Revised

--- Projects/FuYue/CP/Urban_data/data.m
+++ Projects/FuYue/CP/Urban_data/data.m
@@ -1,10 +1,10 @@
 clear all 
 close all
 
-C = csv2cell('urban_pop.csv');
+C = csv2cell('largest_city_pop.csv');
 C1 = C(:,1);
 
-fid = fopen('Urban_data.csv', 'wt');
+fid = fopen('large_data.csv', 'wt');
 
 i=1960;
 
@@ -13,8 +13,8 @@ fprintf(fid,'id,Year,cases\n');
 for j = 2:1:51
     Co = C(:,j);
     Co = cell2mat(Co);
-    for m = 1:1:206
-        fprintf(fid,'%s,%d,%3.2f\n',C1{m},i,Co(m));
+    for m = 1:1:size(C1)
+        fprintf(fid,'%s,%d,%f\n',C1{m},i,Co(m));
     end
     i = i+1;
 end
