d239abe9563a8363b2b0931c6b8715c3252b72dc
Tianyi <wty1990000@yeah.net>
2014-07-07 13:41:13 +0800

    NRF

--- Projects/Qt_learning/MainWindow/mainwindow.cpp
+++ Projects/Qt_learning/MainWindow/mainwindow.cpp
@@ -6,6 +6,21 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+	createActions();
+}
+
+void MainWindow::createActions()
+{
+	ui->action_New->setStatusTip(tr("Create a new Spreadsheat file"));
+	connect(ui->action_New, SIGNAL(ui->action_New->triggered()), this, SLOT(newFile()));
+	
+	ui->action_Save->setStatusTip(tr("Save the spreadsheat to file"));
+	connect(ui->action_Save, SIGNAL(ui->action_Save->triggered()),this,SLOT(saveFile()));
+	
+	ui->actionSave_As->setStatusTip(tr("Save as"));
+	connect(ui->actionSave_As,SIGNAL(ui->actionSave_As->triggered()),this,SLOT(saveAs()));
+
+	
 }
 
 MainWindow::~MainWindow()
--- Projects/Qt_learning/MainWindow/mainwindow.h
+++ Projects/Qt_learning/MainWindow/mainwindow.h
@@ -14,9 +14,40 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+protected:
+	void closeEvent(QCloseEvent *event);
+
+//Slots
+private slots:
+	void newFile();
+	void open();
+	bool save();
+	bool saveAs();
+	void find();
+	void goToCell();
+	void sort();
+	void about();
+	void openRecentFile();
+	void updateStatusBar();
+	void spreadsheetModified();
+private:
+	void createActions();
+	void createMenus();
+	void createContextMenu();
+	void createToolBars();
+	void readSettings();
+	void writeSettings();
+	bool okToContinue();
+	bool loadFile(const QString &fileName);
+	bool saveFile(const QString &fileName);
+	void setCurrentFile(const QString &fileName);
+	void updateRecentFileActions();
+	QString strippedName(const QString &fullFileName);
 
 private:
     Ui::MainWindow *ui;
+	enum{MaxRecentFiles = 5};
+	QAction *recentFileActions[MaxRecentFiles];
 };
 
 #endif // MAINWINDOW_H
